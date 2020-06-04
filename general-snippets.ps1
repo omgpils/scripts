741395585eceafc60703a5cfed1dddaa006cffe8
omgpils <omgpils@users.noreply.github.com>
2020-06-05 00:49:29 +0200

    Update general-snippets.ps1

--- general-snippets.ps1
+++ general-snippets.ps1
@@ -18,6 +18,35 @@ Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveActive -Val
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name ScreenSaveTimeOut -Value 60
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name scrnsave.exe -Value "c:\windows\system32\mystify.scr"
 
+#
+
+#ip settings
+$IP = "10.0.2.200"
+$MaskBits = 24 # This means subnet mask = 255.255.255.0
+$Gateway = "10.0.2.1"
+$IPType = "IPv4"
+
+#find first adapter that is up
+$upadapter = Get-NetAdapter | ? { $_.Status -eq "Up" } | Select-Object -First 1 | Select-Object -ExpandProperty ifIndex
+#reset settings
+Remove-NetIPAddress -InterfaceIndex $upadapter -AddressFamily $IPType -Confirm:$false
+
+#adapter settings
+Get-NetAdapter -InterfaceIndex $upadapter | `
+New-NetIPAddress -AddressFamily $IPType `
+				 -IPAddress $ip `
+				 -PrefixLength $MaskBits `
+				 -DefaultGateway $Gateway
+
+#dns
+Set-DnsClientServerAddress -InterfaceIndex $upIndex -ServerAddresses ("10.0.0.2", "10.0.0.3")
+
+#network, disable ipv6
+Get-NetAdapterBinding -ComponentID 'ms_tcpip6' | disable-NetAdapterBinding -ComponentID ms_tcpip6 -PassThru
+
+#disable inactive adapters
+Get-NetAdapter | ? { $_.status -eq "Disconnected" } | Disable-NetAdapter -Confirm:$false
+
 #list installed software
 Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
 
