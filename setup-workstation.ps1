d933c19602df7a548731d210ab91ce32ab5df19b
omgpils <omgpils@users.noreply.github.com>
2020-06-05 20:39:18 +0200

    Update setup-workstation.ps1

--- setup-workstation.ps1
+++ setup-workstation.ps1
@@ -44,3 +44,53 @@ Get-NetAdapterBinding -ComponentID 'ms_tcpip6' | disable-NetAdapterBinding -Comp
 
 #disable inactive adapters
 Get-NetAdapter | ? { $_.status -eq "Disconnected" } | Disable-NetAdapter -Confirm:$false
+
+###
+
+#WSL
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile WSLUpdate.msi -UseBasicParsing
+msiexec.exe /package WSLUpdate.msi /quiet
+wsl --set-default-version 2
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile Ubuntu.appx -UseBasicParsing
+Add-AppxPackage .\Ubuntu.appx
+
+#Fontes:
+#https://docs.microsoft.com/pt-br/windows/wsl/install-win10
+#https://docs.microsoft.com/en-us/windows/wsl/install-manual
+
+#CHOCOLATEY
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install microsoft-windows-terminal microsoft-teams vscode atom git terraform awscli lxc multipass nano nmap wget curl
+#Fontes:
+#https://chocolatey.org/install
+
+POWERSHELL
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.0.1/PowerShell-7.0.1-win-x64.msi -OutFile PowerShell.msi -UseBasicParsing
+msiexec.exe /package PowerShell.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+#OU
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.0.1/PowerShell-7.0.1-win-x64.msix -OutFile PowerShell.msix -UseBasicParsing
+Add-AppxPackage PowerShell.msix
+#POWERSHELL CORE
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v6.2.5/PowerShell-6.2.5-win-x64.msi -OutFile PowerShellCore.msi -UseBasicParsing
+msiexec.exe /package PowerShellCore.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=1 REGISTER_MANIFEST=1
+#OU
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v6.2.5/PowerShell-6.2.5-win-x64.msix -OutFile PowerShellCore.msix -UseBasicParsing
+Add-AppxPackage PowerShellCore.msix
+#Fontes:
+#https://docs.microsoft.com/pt-br/powershell/scripting/install/installing-powershell-core-on-windows?view=powershell-7
+#https://github.com/PowerShell/PowerShell/releases
+#https://devblogs.microsoft.com/powershell/powershell-core-6-0-generally-available-ga-and-supported/
+
+#WINGET
+Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v0.1.4331-preview/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile Winget.appx -UseBasicParsing
+Add-AppxPackage .\Winget.appx
+#Fontes:
+https://github.com/microsoft/winget-cli/blob/master/README.md
+https://github.com/microsoft/winget-cli/releases/tag/v0.1.4331-preview
+
+#MICROSOFT EDGE
+Invoke-WebRequest -Uri http://dl.delivery.mp.microsoft.com/filestreamingservice/files/1fc0c5fe-c1f5-4879-a43c-515d9f731444/MicrosoftEdgeEnterpriseX64.msi  -OutFile MicrosoftEdge.msi -UseBasicParsing
+msiexec.exe /package MicrosoftEdge.msi /quiet
